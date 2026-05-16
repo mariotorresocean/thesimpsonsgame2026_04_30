1c534ebdd888f26fa73683b9dd918d9515f561e0
Prof. Mário Torres <mario.torres@oceanbrasil.com>
2026-05-16 09:33:20 -0400

    endpoint de todos os personagens

--- app/src/main/java/com/oceanbrasil/thesimpsonsgame/SimpsonsApi.kt
+++ app/src/main/java/com/oceanbrasil/thesimpsonsgame/SimpsonsApi.kt
@@ -2,11 +2,26 @@ package com.oceanbrasil.thesimpsonsgame
 
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface SimpsonsApi {
     @GET("api/character/{id}")
     suspend fun getCharacter(@Path("id") id: Int): CharacterDto
+    @GET("api/character")
+    suspend fun getCharacters(@Query("page") page: Int): CharactersResponse
 }
+
+data class CharactersResponse(
+    val info: PageInfo,
+    val results: List<CharacterDto>
+)
+
+data class PageInfo(
+    val count: Int,
+    val pages: Int,
+    val next: String?,
+    val prev: String?
+)
 
 data class CharacterDto(
     // id, name, status, portrait_path
